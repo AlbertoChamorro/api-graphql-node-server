e734f991016d8d1bb6bed20a012aa232750bb5fd
Alberto Chamorro <albertchc04@gmail.com>
2018-06-23 13:10:50 -0600

    variant teacher resolve in each course

--- schema.js
+++ schema.js
@@ -59,6 +59,13 @@ const resolvers = {
                 }
             ]
         }
+    },
+    Course: {
+        teacher: () => {
+            return {
+                name: "Alberto Martin"
+            }
+        }
     }
 }
 
